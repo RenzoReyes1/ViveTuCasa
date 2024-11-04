0b2e5ae9d1c48f1389d07dbb12deee8d6a6feb03
DanielSologurenFalcon <daniel.sologuren.falcon@gmail.com>
2024-11-03 22:50:16 -0500

    Refactorización de DetalleVenta: Uso de parámetros SQL, manejo de excepciones y liberación de recursos
    Refactorización de DetalleVenta para mejorar seguridad y eficiencia:
    
    Se usan parámetros en las consultas SQL para evitar inyección de código.
    Bloque finally asegura la liberación de recursos de la base de datos.
    Se agrega manejo de errores para informar fallas al cargar datos.
    Configuración de columnas de la tabla separada en un método específico.

--- SisVentas/src/Presentacion/DetalleVenta.java
+++ SisVentas/src/Presentacion/DetalleVenta.java
@@ -5,146 +5,97 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
 
 public class DetalleVenta extends java.awt.Dialog {
 
     DefaultTableModel tableModel = new DefaultTableModel();   
-    
-    private ResultSet rs;
-    private PreparedStatement ps;
-    private Connection cnn;
-    
+
+    private Connection cnn = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
     public static int id_venta = 0;
-    
+
     public DetalleVenta(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        
+
         this.setLocationRelativeTo(null);
-        
+
+        configurarTabla();
+        cargarTable(id_venta);
+    }
+
+    private void configurarTabla() {
         tblDetalleVentas.setModel(tableModel);
         tableModel.addColumn("ID");
         tableModel.addColumn("CANTIDAD");
         tableModel.addColumn("DESCRIPCIÓN");
         tableModel.addColumn("PRECIO");
         tableModel.addColumn("IMPORTE");
-        
-        cargarTable(id_venta);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    private void cargarTable(int id_venta) {
+        String query = "SELECT detalle_ventas.id_detalle_venta, detalle_ventas.cantidad, productos.nombre_producto, "
+                     + "detalle_ventas.precio, detalle_ventas.importe "
+                     + "FROM detalle_ventas "
+                     + "JOIN ventas ON ventas.id_venta = detalle_ventas.id_venta "
+                     + "JOIN productos ON productos.id_producto = detalle_ventas.id_producto "
+                     + "WHERE detalle_ventas.id_venta = ?";
 
-        jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblDetalleVentas = new javax.swing.JTable();
-
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosing(java.awt.event.WindowEvent evt) {
-                closeDialog(evt);
-            }
-        });
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        tblDetalleVentas.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(tblDetalleVentas);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 863, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(65, 65, 65)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    public void cargarTable(int id_venta)
-    {
-        try
-        {
+        try {
             cnn = Conexion.MySQL();
-            ps = cnn.prepareStatement("SELECT detalle_ventas.id_detalle_venta, detalle_ventas.cantidad, productos.nombre_producto, detalle_ventas.precio,  detalle_ventas.importe "
-                    + "                     FROM detalle_ventas JOIN ventas ON ventas.id_venta = detalle_ventas.id_venta "
-                    + "                     JOIN productos ON productos.id_producto = detalle_ventas.id_producto "
-                    + "                     WHERE detalle_ventas.id_venta = '" + id_venta + "'");
+            ps = cnn.prepareStatement(query);
+            ps.setInt(1, id_venta);
             rs = ps.executeQuery();
-            
-            tableModel.setRowCount(0);
-            
-            while (rs.next())
-            {
-                Object Datos[] =
-                {
-                    rs.getInt(1),
-                    rs.getInt(2),
-                    rs.getString(3),
-                    rs.getDouble(4),
-                    rs.getDouble(5)
-                };    
-                
-                tableModel.addRow(Datos);
+
+            tableModel.setRowCount(0); // Limpiar la tabla antes de llenarla
+
+            while (rs.next()) {
+                Object[] datosFila = {
+                    rs.getInt("id_detalle_venta"),
+                    rs.getInt("cantidad"),
+                    rs.getString("nombre_producto"),
+                    rs.getDouble("precio"),
+                    rs.getDouble("importe")
+                };
+                tableModel.addRow(datosFila);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error al cargar detalles de la venta: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            // Cerrar recursos para evitar fugas de memoria
+            try {
+                if (rs != null) rs.close();
+                if (ps != null) ps.close();
+                if (cnn != null) cnn.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
-        
-        catch (Exception ex) {}
     }
-    
-    private void closeDialog(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_closeDialog
+
+    private void closeDialog(java.awt.event.WindowEvent evt) {
         setVisible(false);
         dispose();
-    }//GEN-LAST:event_closeDialog
+    }
 
     public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                DetalleVenta dialog = new DetalleVenta(new java.awt.Frame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            DetalleVenta dialog = new DetalleVenta(new java.awt.Frame(), true);
+            dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+            dialog.setVisible(true);
         });
     }
 
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblDetalleVentas;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration
 }
